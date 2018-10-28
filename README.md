94d2c56231143046f5f68ba7352b3eea23c3bb39
Matheus <entry_2015@mcarvalhor.com>
2018-10-28 15:33:55 -0300

    Adicionado as funcionalidades /feedback e /members; README atualizado

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # GaneshICMC-Bot
+Este robô para Telegram possibilita que os membros e a coordenadoria do Ganesh mantenham algumas de suas atividades e cronograma melhor organizados.
+
+## config.json
+Para pleno funcionamento, é necessário construir um arquivo config.json com a seguinte estrutura:
+	- "subscribers": lista de canais que recebem os broadcasts;
+	- "authorized": lista de usuários do Telegram administradores;
+	- "interval": ?;
+	- "key": chave da Telegram API;
+	- "membersDriveLink": link do Google Drive para download da tabela CSV dos membros do Ganesh (formato: https://docs.google.com/spreadsheet/ccc?output=csv&key=FORM-KEY]);
+	- "feedbackDriveLink": link do Google Drive para download da tabela CSV dos Feedbacks recebidos (formato: https://docs.google.com/spreadsheet/ccc?output=csv&key=FORM-KEY]).
+}
--- app.py
+++ app.py
@@ -6,9 +6,16 @@ import json
 import datetime
 import threading
 import time
+import csv
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ParseMode
+from io import StringIO
+
+
+# Variáveis pré-definidas
+
+CSV_COMMA = ',' # Delimitador do arquivo CSV
 
 
 class App:
@@ -29,6 +36,8 @@ class App:
 		self.dispatcher.add_handler(CommandHandler('subscribe', self.subscribe))
 		self.dispatcher.add_handler(CommandHandler('unsubscribe', self.unsubscribe))
 		self.dispatcher.add_handler(CommandHandler('bcast', self.broadcast))
+		self.dispatcher.add_handler(CommandHandler('feedback', self.feedback))
+		self.dispatcher.add_handler(CommandHandler('members', self.members))
 		self.dispatcher.add_handler(CommandHandler('auth', self.authorize))
 		self.dispatcher.add_handler(CommandHandler('deauth', self.deAuthorize))
 		#self.dispatcher.add_handler(CommandHandler('upcoming', self.upcoming))
@@ -46,6 +55,8 @@ class App:
 			self.authorized = set(o['authorized']) if 'authorized' in o else set()
 			self.interval = o['interval'] if 'interval' in o else 300
 			self.key = o['key']
+			self.membersDriveLink = o['membersDriveLink']
+			self.feedbackDriveLink = o['feedbackDriveLink']
 
 	def save(self):
 		with open('config.json', 'w') as f:
@@ -54,6 +65,8 @@ class App:
 				'authorized': list(self.authorized),
 				'interval': self.interval,
 				'key': self.key,
+				'membersDriveLink': self.membersDriveLink,
+				'feedbackDriveLink': self.feedbackDriveLink,
 			}, f, indent=4)
 
 	def run(self):
@@ -82,6 +95,8 @@ class App:
 		if authUser == 1:
 			msg += "\n__**Admin Only**__\n"
 			msg += "/bcast `Mensagem` - Manda a mensagem para todos os usuários inscritos.\n"
+			msg += "/members - Mostra estatísticas dos membros do Ganesh.\n"
+			msg += "/feedback `Filtro` - Mostra o feedback para a atividade filtrada (o filtro pode ser uma data, por exemplo).\n"
 			msg += "/auth `Username` - Autoriza o usuário com @ `Username` a mandar broadcast.\n"
 			msg += "/deauth `Username` - Desautoriza o usuário com @ `Username` a mandar broadcast.\n"
 
@@ -180,6 +195,130 @@ class App:
 		else:
 			for subscriber in self.subscribers:
 				bot.send_message(chat_id=subscriber, text=sendMsg, parse_mode=ParseMode.MARKDOWN)
+
+	def members(self, bot, update): # Calcula o número de membros por curso e ano
+		message = update.message
+		if not message.from_user.username in self.authorized:
+			bot.send_message(chat_id=message.chat_id, text="Usuário não autorizado :(")
+			return
+
+		try:
+			with StringIO(urllib.request.urlopen(self.membersDriveLink).read().decode('utf-8')) as mStream:
+				csvOutput = csv.reader(mStream, delimiter=CSV_COMMA)
+				csvLines = list(csvOutput)
+		except Exception as exc:
+				print(exc)
+				bot.send_message(chat_id=message.chat_id, text="Poxa, não consegui baixar a tabela do Drive :(")
+				return
+
+		del csvLines[0] # Remove a coluna header
+		courses = { }
+		nMembers = 0
+
+		for member in csvLines:
+			if member[8] == 'Inativo': # Ignorar membros inativos no cálculo
+				continue
+			nMembers += 1
+			if not member[3] in courses:
+				courses[member[3]] = [0, {}]
+			courses[member[3]][0] += 1
+			if member[4] in courses[member[3]][1]:
+				courses[member[3]][1][member[4]] += 1
+			else:
+				courses[member[3]][1][member[4]] = 1
+
+		outputResults = "<strong>== Membros do Ganesh -> " + str(nMembers) + " ==</strong>\n\n"
+		for course in sorted(courses):
+			outputResults += "<strong>~ " + course + " -> " + str(courses[course][0]) + " (" + str(round(courses[course][0]*100/nMembers, 2)) + "%) ~</strong>\n"
+			for year in sorted(courses[course][1]):
+				outputResults += "\t" + year + ": " + str(courses[course][1][year]) + " (" + str(round(courses[course][1][year]*100/nMembers, 2)) + "%)\n"
+			outputResults += "\n"
+
+		bot.send_message(chat_id=message.chat_id, text=outputResults, parse_mode='HTML')
+
+	def feedback(self, bot, update): # Calcula o feedback das atividades ou reuniões
+		message = update.message
+		messageList = message.text.split(' ', 1)
+		filterFeedback = False
+		if len(messageList) > 1:
+			queryFilter = messageList[1]
+			filterFeedback = True
+
+		if not message.from_user.username in self.authorized:
+			bot.send_message(chat_id=message.chat_id, text="Usuário não autorizado :(")
+			return
+
+		try:
+			with StringIO(urllib.request.urlopen(self.feedbackDriveLink).read().decode('utf-8')) as mStream:
+				csvOutput = csv.reader(mStream, delimiter=CSV_COMMA)
+				csvLines = list(csvOutput)
+		except Exception as exc:
+				print(exc)
+				bot.send_message(chat_id=message.chat_id, text="Poxa, não consegui baixar a tabela do Drive :(")
+				return
+
+		del csvLines[0] # Remove a coluna header
+		data = { }
+
+		for response in csvLines:
+			if not response[1] in data:
+				data[response[1]] = [0, {}, {}, {}, [], []]
+			data[response[1]][0] += 1
+			if response[2] in data[response[1]][1]:
+				data[response[1]][1][response[2]] += 1
+			else:
+				data[response[1]][1][response[2]] = 1
+			if response[3] in data[response[1]][2]:
+				data[response[1]][2][response[3]] += 1
+			else:
+				data[response[1]][2][response[3]] = 1
+			if response[4] in data[response[1]][3]:
+				data[response[1]][3][response[4]] += 1
+			else:
+				data[response[1]][3][response[4]] = 1
+			if response[5]:
+				data[response[1]][4].append(response[5])
+			if response[6]:
+				data[response[1]][5].append(response[6])
+
+		outputResults = ""
+		found = False
+		for dataKey in data:
+			if filterFeedback and queryFilter not in dataKey:
+				continue
+			found = True
+			outputResults += "<strong>== " + dataKey + ": " + str(data[dataKey][0]) + " feedbacks recebidos ==</strong>\n"
+			outputResults += "<em>Gostou do tema da atividade?</em>\n"
+			for liked in sorted(data[dataKey][1]):
+				outputResults += "\t- " + liked + ": " + str(data[dataKey][1][liked]) + " (" + str(round(data[dataKey][1][liked]*100/data[dataKey][0], 2)) + "%)\n"
+			outputResults += "<em>O quanto você entendeu do assunto?</em>\n"
+			scoreSum = 0
+			for score in sorted(data[dataKey][2]):
+				outputResults += "\t- " + score + "/5: " + str(data[dataKey][2][score]) + " (" + str(round(data[dataKey][2][score]*100/data[dataKey][0], 2)) + "%)\n"
+				scoreSum += int(score) * data[dataKey][2][score]
+			outputResults += "\t- média: " + str(round(scoreSum/data[dataKey][0], 1)) + "/5\n"
+			outputResults += "<em>Vale a pena revisar o assunto em reuniões futuras?</em>\n"
+			for review in sorted(data[dataKey][3]):
+				outputResults += "\t- " + review + ": " + str(data[dataKey][3][review]) + " (" + str(round(data[dataKey][3][review]*100/data[dataKey][0], 2)) + "%)\n"
+			outputResults += "<em>Algum assunto que gostaria para próximas atividades ou reuniões?</em>\n"
+			if len(data[dataKey][4]) < 1:
+				outputResults += "\t(nada)\n"
+			for subject in data[dataKey][4]:
+				outputResults += "<pre>" + subject.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;") + "</pre>"
+			outputResults += "<em>Alguma observação ou sugestão de mudança?</em>\n"
+			if len(data[dataKey][5]) < 1:
+				outputResults += "\t(nada)\n"
+			for suggestion in data[dataKey][5]:
+				outputResults += "<pre>" + suggestion.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;") + "</pre>"
+			outputResults += "\n"
+
+		if found:
+			bot.send_message(chat_id=message.chat_id, text=outputResults, parse_mode='HTML', disable_web_page_preview=True)
+		else:
+			if queryFilter is not None:
+				bot.send_message(chat_id=message.chat_id, text="Nada encontrado com <strong>" + queryFilter.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;") + "</strong> :(", parse_mode='HTML', disable_web_page_preview=True)
+			else:
+				bot.send_message(chat_id=message.chat_id, text="Nada encontrado :(")
 
 
 	# CTF Functionalities, to be done in the future.
